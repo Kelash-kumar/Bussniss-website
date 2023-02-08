88615641a32262d35b1497ec626e13b60ee1e973
kelash <kelash.raisal@gmail.com>
2023-02-08 22:42:37 +0500

    js window function added :

--- busni.js
+++ busni.js
@@ -1,7 +1,9 @@
 // let btn=document.getElementById("loginForm");
 
-
 const loginForm = document.getElementById("loginForm");
+window.onload=()=>{
+    loginForm.style.display="none";
+}
 const loginbtn = document.getElementById("loginbtn");
 loginbtn.onclick = function () {
   if (loginForm.style.display !== "none") {
