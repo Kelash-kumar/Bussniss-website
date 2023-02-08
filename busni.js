826c6618797588f5c7cddcb7846fb126ae7cdf2a
kelash <kelash.raisal@gmail.com>
2023-02-08 22:38:51 +0500

    website is ready i have installed the login form

---
+++ busni.js
@@ -0,0 +1,17 @@
+// let btn=document.getElementById("loginForm");
+
+
+const loginForm = document.getElementById("loginForm");
+const loginbtn = document.getElementById("loginbtn");
+loginbtn.onclick = function () {
+  if (loginForm.style.display !== "none") {
+    loginForm.style.display = "none";
+  } else {
+    loginForm.style.display = "block";
+  }
+};
+
+// loginbtn.addEventListener('click',function(){
+// // loginbtn.style.display="block";
+// loginbtn.style.backgroundColor="red";
+// })
